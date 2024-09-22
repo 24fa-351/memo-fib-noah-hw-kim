5d6e75dbe73633c22d5cd73ca87c553301de3cd4
Noah (Hyeonwoo) Kim <84950847+noah-hw-kim@users.noreply.github.com>
2024-09-21 19:56:03 -0700

    updated to use monad and memoization together

--- fib.c
+++ fib.c
@@ -3,27 +3,44 @@
 #include <stdint.h>
 #include <limits.h>
 
-typedef unsigned long long int (*fibFunc)(int);
-unsigned long long int memo[100];
-fibFunc ptr;
+// enum vals (success, failure}
+enum State {
+   SUCCESS = 1,
+   FAILURE = 0
+};
 
+// Monad
+typedef struct { unsigned long long num; enum State state; } ReturnVal;
+typedef ReturnVal (*fibFunc)(int);
 void initializeCache(fibFunc);
-unsigned long long int fib_i(int);
-unsigned long long int fib_r(int);
-unsigned long long int fibWrapper(int);
-int isOverflow(unsigned long long int, unsigned long long int);
+int sumWouldOverflow(unsigned long long, unsigned long long);
+ReturnVal fib_i(int);
+ReturnVal fib_r(int);
+ReturnVal fibWrapper(int);
+
+// global variables
+fibFunc ptr;
+const int memo_size = 100;
+ReturnVal memo[memo_size];
+
 
 void initializeCache(fibFunc chosenFunc) {
    ptr = chosenFunc;
-   memo[2] = 1;
+   for (int i = 0; i < memo_size; i++) {
+      memo[i] = (ReturnVal){ 0, SUCCESS };
+   }
 }
 
-unsigned long long int fib_i(int seq) {
+int sumWouldOverflow(unsigned long long int num1, unsigned long long int num2) {
+   return num2 > ULLONG_MAX - num1;
+}
+
+ReturnVal fib_i(int seq) {
    // base
    if (seq == 1) {
-      return 0;
+      return (ReturnVal){ 0, SUCCESS } ;
    } else if (seq == 2) {
-      return 1;
+      return (ReturnVal){ 1, SUCCESS } ;
    }
 
    unsigned long long int prev = 0;
@@ -31,38 +48,46 @@ unsigned long long int fib_i(int seq) {
 
    // iterate starts from 3rd fib since we already have 1st and 2nd fib
    for (int i = 2; i < seq; i++) {
-      if (isOverflow(prev, cur)) {
-         return ULLONG_MAX;
+      if (sumWouldOverflow(prev, cur)) {
+         printf("overflow occured at %d\n", seq);
+         return (ReturnVal){ 0, FAILURE };
       }
       unsigned long long int nxt = prev + cur;
       prev = cur;
       cur = nxt;
    }
 
-   return cur;
+   return (ReturnVal){ cur, SUCCESS };
 }
 
-unsigned long long int fib_r(int seq) {
+ReturnVal fib_r(int seq) {
    // base case
    if (seq == 1) {
-      return 0;
+      return (ReturnVal){ 0, SUCCESS } ;
    } else if (seq == 2) {
-      return 1;
+      return (ReturnVal){ 1, SUCCESS } ;
    }
 
-   unsigned long long int prev = fibWrapper(seq - 1);
-   unsigned long long int prevprev = fibWrapper(seq - 2);
-
-   if (isOverflow(prev, prevprev)) {
-      return ULLONG_MAX;
+   ReturnVal prev = fibWrapper(seq - 1);
+   if (prev.state == FAILURE) {
+      return prev;
+   }
+   ReturnVal prevprev = fibWrapper(seq - 2);
+   if (prevprev.state == FAILURE) {
+      return prevprev;
    }
 
-   return prev + prevprev;
+   if (sumWouldOverflow(prev.num, prevprev.num)) {
+      printf("overflow occured at %d\n", seq);
+      return (ReturnVal){ 0, FAILURE } ;
+   }
+
+   return (ReturnVal){ prev.num + prevprev.num, SUCCESS } ;
 }
 
-unsigned long long int fibWrapper(int seq) {
+ReturnVal fibWrapper(int seq) {
    // base case
-   if (memo[seq] != 0) {
+   if (memo[seq].num != 0) {
       return memo[seq];
    }
 
@@ -71,21 +96,12 @@ unsigned long long int fibWrapper(int seq) {
    return memo[seq];
 }
 
-int isOverflow(unsigned long long int prev, unsigned long long int cur) {
-   // overflow happens when prev + cur > max_int so we have overflow when cur > max_int - prev
-   if (cur > ULLONG_MAX - prev) {
-      return 1;
-   }
-   return 0;
-}
-
 int main(int argc, char *argv[]) {
    int seq;
    sscanf(argv[1], "%d", &seq);
    char* method = argv[2];
 
-   unsigned long long int result;
-   
+   ReturnVal result;
    
    if (*method == 'r') {
       initializeCache(fib_r);
@@ -99,12 +115,93 @@ int main(int argc, char *argv[]) {
    } else {
       result = fibWrapper(seq);
 
-      if (result == ULLONG_MAX) {
-         printf("overflow occured");
-      } else {
-         printf("%llu", result);
+      if (result.state) {
+         printf("%llu", result.num);
       }
    }
 
    return 0;
 }
+
+// // enum vals (success, failure}
+// enum State {
+//    SUCCESS = 1,
+//    FAILURE = 0
+// };
+
+// // Monad
+// typedef struct { unsigned long long num; enum State state; } ReturnVal;
+// int sumWouldOverflow(unsigned long long, unsigned long long);
+// ReturnVal fib_i(int);
+// ReturnVal fib_r(int);
+
+// unsigned long long int fib_i(int seq) {
+//    // base
+//    if (seq == 1) {
+//       return 0;
+//    } else if (seq == 2) {
+//       return 1;
+//    }
+
+//    unsigned long long int prev = 0;
+//    unsigned long long int cur = 1;
+
+//    // iterate starts from 3rd fib since we already have 1st and 2nd fib
+//    for (int i = 2; i < seq; i++) {
+//       if (isOverflow(prev, cur)) {
+//          return ULLONG_MAX;
+//       }
+//       unsigned long long int nxt = prev + cur;
+//       prev = cur;
+//       cur = nxt;
+//    }
+
+//    return cur;
+// }
+
+// unsigned long long int fib_r(int seq) {
+//    // base case
+//    if (seq == 1) {
+//       return 0;
+//    } else if (seq == 2) {
+//       return 1;
+//    }
+
+//    unsigned long long int prev = fibWrapper(seq - 1);
+//    unsigned long long int prevprev = fibWrapper(seq - 2);
+
+//    if (isOverflow(prev, prevprev)) {
+//       return ULLONG_MAX;
+//    }
+
+//    return prev + prevprev;
+// }
+
+
+
+
+// int main(int argc, char *argv[]) {
+//    int seq;
+//    sscanf(argv[1], "%d", &seq);
+//    char* method = argv[2];
+
+//    ReturnVal result;
+   
+//    if (*method == 'r') {
+//       result = fib_r(seq);
+//    } else if (*method == 'i') {
+//       result = fib_i(seq);
+//    }
+
+//    // nth number starts from 1 not 0
+//    if (seq < 1) {
+//       printf("fibonacci index should be positive");
+//    } else {
+//       if (result.state) {
+//          printf("%llu", result.num);
+//       }
+//    }
+
+//    return 0;
+// }
+
